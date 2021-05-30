14fc1c5c323e286ada9bfd50d88ccf1d70a82d8c
SofiFairyTell <fairytellsblog@gmail.com>
2021-05-31 01:19:19 +0300

    edit add
    - по методу пост все данные в объекты распределяются верно
    - объект не добавляется в таблицу

--- FeedBack/FeedBackMVC/Controllers/HomeController.cs
+++ FeedBack/FeedBackMVC/Controllers/HomeController.cs
@@ -22,5 +22,29 @@ namespace FeedBackMVC.Controllers
             //генерация представления: получаем все объекты из БД в виде списка
             return View(dataContext.Users.ToList());
         }
+
+        //Найти сообщения по id пользователя
+        [HttpGet]
+        public IActionResult SendMessage(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            ViewBag.UserId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string SendMessage(Message message, User user)
+        {
+            dataContext.Users.Add(user);
+            if(message.User == null)
+            {
+                message.User.User_name = user.User_name;
+                message.User.Id = user.Id;
+            }
+            dataContext.Messages.Add(message);
+            dataContext.SaveChangesAsync();
+            return "Ваше сообщение"+ message.User.User_name +"отправлено";
+        }
     }
 }
